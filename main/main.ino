c70040aa5ed1fb3d5d9b6898cd61d93df2dbfb17
Thanapat Kemthong <thanapat.kem@gmail.com>
2025-04-08 10:23:57 +0700

    fix: fix servo angle

--- main/main.ino
+++ main/main.ino
@@ -10,41 +10,57 @@ bool isAtZero = true;    // Flag to track servo position (0 or x degrees)
 // int servoMax_2 = 90;              // Target angle for servo_2 rotation
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);
   pinMode(13, OUTPUT); 
   servo_1.attach(9);
   servo_2.attach(10);
 }
 
-float radToDeg(float radians) {
-  return radians * (180.0 / PI);
-}
+// float radToDeg(float radians) {
+//   return radians * (180.0 / PI);
+// }
 
-void rotateServo(Servo &servo, int startAngle, int endAngle) {
-  isRotating = true;
-  int step = (startAngle < endAngle) ? 1 : -1;
-  for (int angle = startAngle; angle != endAngle + step; angle += step) {
-    servo.write(angle);
-    delay(15); 
-  }
-  isRotating = false;
-}
+// void rotateServo(Servo &servo, int startAngle, int endAngle) {
+//   isRotating = true;
+//   int step = (startAngle < endAngle) ? 1 : -1;
+//   for (int angle = startAngle; angle != endAngle + step; angle += step) {
+//     servo.write(angle);
+//     delay(15); 
+//   }
+//   isRotating = false;
+// }
 
 void closeLid() {
+  isRotating = true;
   for (int i = 0; i < 100; i++) {
-    servo_1.write(radToDeg(SERVO_1_POS[i]));
-    servo_2.write(radtoDeg(SERVO_2_POS[i]));
-    delay(15);
+    servo_1.write(SERVO_1_POS[i]);
+    servo_2.write(SERVO_2_POS[i]);
+
+    Serial.print("servo1:");
+    Serial.print(SERVO_1_POS[i]);
+    Serial.print(",servo2:");
+    Serial.println(SERVO_2_POS[i]);
+    // delay(25);
   }
+  isRotating = false;
+  Serial.println("Closed");
 }
 
 void openLid() {
+  isRotating = true;
   for (int i = 99; i >= 0; i--) {
-    servo_1.write(radToDeg(SERVO_1_POS[i]));
-    servo_2.write(radToDeg(SERVO_2_POS[i]));
-    delay(15);
+    servo_1.write(SERVO_1_POS[i]);
+    servo_2.write(SERVO_2_POS[i]);
+
+    Serial.print("servo1:");
+    Serial.print(SERVO_1_POS[i]);
+    Serial.print(",servo2:");
+    Serial.println(SERVO_2_POS[i]);
+    // delay(25);
   }
+  isRotating = false;
+  Serial.println("Opened");
 }
 
 
@@ -62,8 +78,5 @@ void loop() {
       // rotateServo(servo_2, servoMax_2, 0);
     }
     isAtZero = !isAtZero;
-  } else {
-    Serial.println("Button released");
-    digitalWrite(13, LOW);
   }
 }
