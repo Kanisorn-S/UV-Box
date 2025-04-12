99a74ff1ce2da6179d5ed7a678996ca8e1f03ba0
Kanisorn-S <karamahati@gmail.com>
2025-04-12 22:41:16 +0700

    feat: working

--- test/test.ino
+++ test/test.ino
@@ -7,9 +7,9 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 const int servoCount = 2; // Number of servos you have
-const int servoPins[] = {0, 1};
+const int servoPins[] = {3, 1};
 
-double defaultPositions[servoCount] = {SERVO_1_POS[0], SERVO_2_POS[0]}; // Default position is when lid is closed
+double defaultPositions[servoCount] = {SERVO_1_POS[0], SERVO_2_POS[0]}; // Default position is when lid is open
 double servoAngles[servoCount]; // Array to store current servo angles
 
 bool isRotating = false; // Flag to track if rotation is in progress
@@ -26,7 +26,7 @@ void setup()
   for (int i = 0; i < servoCount; i++)
   {
     servoAngles[i] = defaultPositions[i];
-    pwm.setPWM(servoPins[i], 0, angleToPulse(servoAngles[i]));
+    pwm.setPWM(servoPins[i], 0, myAngleToPulse(servoAngles[i], false, i));
   }
   // servoMax(); // Set servos to minimum position
   setDefaultPositions();
@@ -35,8 +35,8 @@ void setup()
 
 void closeLid() {
   isRotating = true;
-  for (int i = 0; i < 200; i++) {
-    moveServo(0, 1, SERVO_1_POS[i], SERVO_2_POS[i]);
+  for (int i = 0; i < 100; i++) {
+    myMoveServo(0, 1, SERVO_1_POS[i], SERVO_2_POS[i], i);
     // moveServo(1, SERVO_2_POS[i]);
 
     // Serial.print("servo1:");
@@ -51,37 +51,37 @@ void closeLid() {
 
 void openLid() {
   isRotating = true;
-  for (int i = 199; i >= 0; i--) {
-    moveServo(0, 1, SERVO_1_POS[i], SERVO_2_POS[i]);
+  for (int i = 99; i >= 0; i--) {
+    myMoveServo(0, 1, SERVO_1_POS[i], SERVO_2_POS[i], i);
     // moveServo(1, SERVO_2_POS[i]);
 
     // Serial.print("servo1:");
     // Serial.print(SERVO_1_POS[i]);
     // Serial.print(",servo2:");
     // Serial.println(SERVO_2_POS[i]);
-    delay(25);
+    // delay(25);
   }
   isRotating = false;
   Serial.println("Opened");
 }
 
-// void servoMin() {
-//   isRotating = true;
-//   for (int i = 0; i < servoCount; i++) {
-//     moveServo(i, 0);
-//   }
-//   isRotating = false;
-//   Serial.println("Min");
-// }
+void servoMin() {
+  isRotating = true;
+  for (int i = 0; i < servoCount; i++) {
+    moveServo(i, 0);
+  }
+  isRotating = false;
+  Serial.println("Min");
+}
 
-// void servoMax() {
-//   isRotating = true;
-//   for (int i = 0; i < servoCount; i++) {
-//     moveServo(i, 180);
-//   }
-//   isRotating = false;
-//   Serial.println("Max");
-// }
+void servoMax() {
+  isRotating = true;
+  for (int i = 0; i < servoCount; i++) {
+    moveServo(i, 180);
+  }
+  isRotating = false;
+  Serial.println("Max");
+}
 
 void loop()
 {
@@ -99,29 +99,49 @@ void loop()
   }
 }
 
-void moveServo(int servoIndex_1, int servoIndex_2, double targetAngle_1, double targetAngle_2)
+void myMoveServo(int servoIndex_1, int servoIndex_2, double targetAngle_1, double targetAngle_2, int i)
 {
-      pwm.setPWM(servoPins[servoIndex_2], 0, angleToPulse(targetAngle_2));
-      pwm.setPWM(servoPins[servoIndex_1], 0, angleToPulse(targetAngle_1));
+      pwm.setPWM(servoPins[servoIndex_2], 0, myAngleToPulse(targetAngle_2, false, i));
+      pwm.setPWM(servoPins[servoIndex_1], 0, myAngleToPulse(targetAngle_1, true, i));
+      delay(15);
+}
 
-
+void moveServo(int servoIndex, int targetAngle)
+{
+  // Smoothly move the servo to the target angle
+  for (int i = servoAngles[servoIndex]; i != targetAngle; i += (targetAngle > i) ? 1 : -1)
+  {
+    servoAngles[servoIndex] = i;
+    pwm.setPWM(servoPins[servoIndex], 0, angleToPulse(i));
+    delay(10); // Adjust the delay for smoother motion
+  }
 }
 
 void setDefaultPositions()
 {
   // Set all servos to their default positions
-  moveServo(0, 1, defaultPositions[0], defaultPositions[1]);
+  myMoveServo(0, 1, defaultPositions[0], defaultPositions[1], 0);
   Serial.println("Home");
 }
 
-double angleToPulse(double angle)
+double myAngleToPulse(double angle, bool debug, int i)
 {
   // Convert angle to servo pulse length
-  Serial.println(int(myMap(angle)));
+  if (debug) 
+  {
+    Serial.println(i + int(myMap(angle)));
+  }
   return myMap(angle);
 }
 
 double myMap(double angle)
 {
   return SERVOMIN + (((angle - 0) * (SERVOMAX - SERVOMIN)) / (180));
+}
+
+int angleToPulse(int angle)
+{
+  // Convert angle to servo pulse length
+  Serial.println(map(angle, 0, 180, SERVOMIN, SERVOMAX));
+  return map(angle, 0, 18, SERVOMIN, SERVOMAX);
 }
